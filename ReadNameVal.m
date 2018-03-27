d7f2dbadd5fd6478240236a3356284f8af1e99d1
Heather Ratcliffe <hratcliffe.glasgow@gmail.com>
2018-03-27 17:32:35 +0100

    Read name-value pairs into structure
    Designed for files like EPOCHs deck.status, creates
    a struct with one key per item in file

---
+++ ReadNameVal.m
@@ -0,0 +1,43 @@
+
+%% Reading functions
+function constants = ReadNameVal(dir, file)
+
+  if( nargin < 2)
+    file = 'const.status'
+  end
+  if(nargin < 1)
+      dir = './Data'
+  end
+  name = strcat(dir, '/',  file);
+  constants.file = name;
+  fid = fopen(name);
+  if fid ~= -1
+      tline = fgetl(fid);
+      while ischar(tline)
+          content = parse_name_val(tline);
+        if isfield(content, 'n') && isfield(content, 'v')
+          constants.(content.n) = content.v;
+        end
+        tline = fgetl(fid);
+      end
+  end
+end
+function nv = parse_name_val(str,delim)
+
+  if nargin < 2
+    delim = '=';
+  end
+  parts = strsplit(str, delim);
+  if size(parts, 2) == 2
+    nv.n = char(strtrim(parts(1)));
+    val = strtrim(parts(2));
+    vald = str2double(val);
+    if vald == vald
+      nv.v = vald;
+    end
+  else
+    nv = 0;
+  end
+end
+
+
